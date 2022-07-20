10efe12e6ae00df83f61ad5be79e68da1e7aaf7d
Edward <edward.yuan@iotpay.ca>
2022-07-20 11:52:00 -0700

    up

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,13 @@
-import '@hotwired/turbo-rails';
-import 'controllers';
-import LocalTime from 'local-time';
+import "@hotwired/turbo-rails";
+import "controllers";
+import LocalTime from "local-time";
 LocalTime.start();
+
+// --------------------------------------------------------------------
+// sprockets-rails import is not working with importmap-rails
+// as in assets/javascript/application.js. I can not find a solution
+//
+// Ask instructor how to get them work together or should ditch sprockets
+// In the demo projects, only importmap is used
+// As for newer developement, webpack is recommanded
+// --------------------------------------------------------------------
