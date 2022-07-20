065d62a8ad59b84488a2c657cd6bc9f8ff894ef3
Edward <edward.yuan@iotpay.ca>
2022-07-20 12:20:42 -0700

    fix import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,11 +3,29 @@ import "controllers";
 import LocalTime from "local-time";
 LocalTime.start();
 
+import "popper";
+import "bootstrap";
+
 // --------------------------------------------------------------------
 // sprockets-rails import is not working with importmap-rails
 // as in assets/javascript/application.js. I can not find a solution
 //
 // Ask instructor how to get them work together or should ditch sprockets
 // In the demo projects, only importmap is used
-// As for newer developement, webpack is recommanded
+// In this project, I added
+// Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
+// to assets.rb and importmap.rb then
+// used importmap
 // --------------------------------------------------------------------
+
+//= require jquery3
+
+//= require popper
+
+//= require bootstrap
+
+//= require rails-ujs
+
+//= require turbolinks
+
+//= require_tree .
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "local-time", to: "https://ga.jspm.io/npm:local-time@2.1.0/app/assets/javascripts/local-time.js"
+
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
